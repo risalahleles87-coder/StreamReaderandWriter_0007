05241fbf536f79abf4fb1a3021ac17a9f21b6640
risalahlele87-coder <risalahleles87@gmail.com>
2026-06-01 15:01:22 +0700

    Menambahkan class TokoElektronik dengan fitur pengambilan produk dan penanganan exception

--- tugas.cpp
+++ tugas.cpp
@@ -7,3 +7,33 @@
 using namespace std;
 
 
+// KELAS TOKO ELEKTRONIK (OOP)
+
+class TokoElektronik {
+private:
+    array<string, 3> etalase;
+
+public:
+    // Constructor
+    TokoElektronik() {
+        etalase[0] = "Laptop Asus";
+        etalase[1] = "Smartphone Samsung";
+        etalase[2] = "Printer Epson";
+    }
+
+    // Method mengambil produk
+    string ambilProduk(size_t nomorRak) {
+        try {
+            return etalase.at(nomorRak);
+        }
+        catch (out_of_range&) {
+            throw string(
+                "Gagal Mengambil Barang : Rak nomor " +
+                to_string(nomorRak) +
+                " kosong atau tidak tersedia!"
+            );
+        }
+    }
+};
+
+
