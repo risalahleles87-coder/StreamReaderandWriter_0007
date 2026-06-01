3e617f4b8076f634fb699551c9c0a27cab04fafe
risalahlele87-coder <risalahleles87@gmail.com>
2026-06-01 10:15:09 +0700

    Menambahkan proses penutupan file serta membuka kembali file untuk dibaca

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -31,4 +31,15 @@ int main()
         //menulis dan memasukkan nilai dari baris ke dalam file
         outfile << baris << endl;
     }
-    
+    //menutup file apabila telah selesai di tulis
+    outfile.close();
+
+    //membuka file dalam mode membaca
+    ifstream infile;
+
+    //menunjuk ke sebuah file dengan ekstensi .txt
+    infile.open(NamaFile + ".txt", ios::in);
+
+    cout << endl << ">= Membuka dan membaca file" << endl;
+    //kondisi jika file ada
+    if (infile.is_open())
